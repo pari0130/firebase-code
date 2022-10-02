84ee8f697a71c5d60c7d6f0e6a78e5012d1d357c
coheim <pari0130@gmail.com>
2022-10-02 22:20:09 +0900

    정렬 테스트 추가

--- firebase-get-v1.py
+++ firebase-get-v1.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import logging
 import urllib3
+from google.cloud.firestore_v1.base_query import BaseQuery
 
 http = urllib3.PoolManager()
 logger = logging.getLogger()
@@ -13,7 +14,7 @@ firebase_admin.initialize_app(cred, {
 })
 db = firestore.client()
 
-docs = db.collection(u'items').where(u'prdkind', u'==', u'커피').stream()
+docs = db.collection(u'items').where(u'prdkind', u'==', u'커피').order_by(u'prdlstReportNo', direction=BaseQuery.DESCENDING).limit(10).stream()
 
 for doc in docs:
     print(f'{doc.id} => {doc.to_dict()}')
